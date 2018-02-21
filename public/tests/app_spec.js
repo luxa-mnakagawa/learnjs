5d64f695a73b619bebe91e2bcd079dff01d6fe4d
Minoru Nakagawa <minoru.nakagawa@luxa.co.jp>
2018-02-21 19:58:18 +0900

    2.1節完了

---
+++ public/tests/app_spec.js
@@ -0,0 +1,6 @@
+describe('LearnJS', function() {
+    it('can show a problem view', function() {
+        learnjs.showView('#problem view');
+        expect($('.view-container .problem-view').length).toEqual(1);
+    });
+});
