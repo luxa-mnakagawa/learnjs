9561b1cecb67503ab082f9c159eeecfcf660e877
Minoru Nakagawa <minoru.nakagawa@luxa.co.jp>
2018-03-08 10:17:06 +0900

    3.1.0節完了

--- public/app.js
+++ public/app.js
@@ -1,12 +1,19 @@
 'use strict';
 var learnjs = {};
 
-learnjs.problemView = function(problemNumber) {
+learnjs.problemView = function(data) {
+    var problemNumber = parseInt(data, 10);
     var view = $('.templates .problem-view').clone();
-    view.find('.title').text('Problem #' + problemNumber + ' Coming soon!')
-    console.log('view:' + view.html());
+    view.find('.title').text('Problem #' + problemNumber);
+    learnjs.applyObject(learnjs.problems[problemNumber - 1], view);
     return view;
 }
+
+learnjs.applyObject = function(obj, elem) {
+    for (var key in obj) {
+        elem.find('[data-name="' + key + '"]').text(obj[key]);
+    }
+};
 
 learnjs.showView = function(hash) {
     var routes = {
@@ -16,7 +23,6 @@ learnjs.showView = function(hash) {
     var viewFn = routes[hashParts[0]];
     if (viewFn) {
         $('.view-container').empty().append(viewFn(hashParts[1]));
-        console.log("view-container:" + $('.view-container').html());
     }
 };
 
@@ -29,11 +35,11 @@ learnjs.appOnReady = function() {
 
 learnjs.problems = [
     {
-        desciption: "What is truth?",
+        description: "What is truth?",
         code: "function problem() { return __; }"
     },
     {
-        desciption: "Simple Math",
+        description: "Simple Math",
         code: "function problem() { return 42 === 6 * __; }"
     }
 ];
